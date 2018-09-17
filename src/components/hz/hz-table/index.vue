5fdbeb2bff226e024c486f3c853c037ae51bbd54
Jamter Tsai <121978614@qq.com>
2018-09-17 17:28:13 +0800

    feat(components):增加hz-table组件

---
+++ src/components/hz/hz-table/index.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="hz-table">
+        <div class="title">{{ options.title }}</div>
+        <table>
+            <tr>
+                <th v-for="(thItem, index) in options.headers">{{ thItem.label }}</th>
+            </tr>
+            <tr v-for="(tdItem) in options.rows">
+                <td v-for="(thItem, index) in options.headers">
+                    {{ tdItem[thItem.name] }}
+                </td>
+            </tr>
+        </table>
+    </div>
+</template>
+<script>
+    export default {
+        props: {
+            options: {
+                type: Object,
+                default: ()=>{
+                    return {
+                        title: '',
+                        headers: [], // {name: 'dssd', label: 'dssd'}
+                        datas: []
+                    }
+                }
+            }
+        },
+        data() {
+            return {
+            }
+        }
+    }
+</script>
+<style lang="less">
+    .hz-table {
+        .title:  {
+            
+        }
+    }
+</style>
