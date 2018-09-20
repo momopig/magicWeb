a24e7d63f444d24fd21bdc3aa3f9a5c3b4344d1f
Jamter Tsai <121978614@qq.com>
2018-09-20 18:23:33 +0800

    更新写法

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -12,7 +12,10 @@ const path = require('path');
 const package = require('../package.json');
 
 fs.open('./build/env.js', 'w', function(err, fd) {
-    const buf = 'export default "production";';
+    // const buf = 'export default "production";';
+    // fs.write(fd, buf, 0, buf.length, 0, function(err, written, buffer) {});
+
+    const buf = Buffer.from('export default "production";');
     fs.write(fd, buf, 0, buf.length, 0, function(err, written, buffer) {});
 });
 
