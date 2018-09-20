9abda53753afdf077e2b116b14e3c4f4bef93294
Zeno Liu <liuchuan@stargraph.cn>
2018-09-20 16:03:29 +0800

    更新代码

--- src/views/main/children/intelligence/detail/index.vue
+++ src/views/main/children/intelligence/detail/index.vue
@@ -1,6 +1,10 @@
 <template>
-    <div class="nav">
-        detail
+    <div class="detail-page">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="/#/intelligence/nav">首页</a></li>
+            <li class="breadcrumb-item"><a href="/#/intelligence/search">金融知识检索</a></li>
+            <li class="breadcrumb-item active">详情</li>
+        </ol>
     </div>
 </template>
 <script>
@@ -10,4 +14,8 @@
     };
 </script>
 <style lang="less">
+    .detail-page{
+        width: 980px;
+        margin: 0 auto;
+    }
 </style>
