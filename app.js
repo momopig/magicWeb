60dad42dac58c448586dcf9048221fc67d70b81c
wudongpeng@haizhi.com <chenshuangyan@haizhi.com>
2018-10-11 19:06:20 +0800

    add app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var app = express()
+var path = require('path')
+var proxyMiddleware = require('http-proxy-middleware')
+
+const apiProxy = proxyMiddleware('/bqd', {
+    target: 'http://192.168.1.135:8080'
+  })
+app.use(apiProxy) // api子目录下的都是用代理
+
+//app.get('/', function (req, res) {
+//  res.sendFile(path.join(__dirname + '/index.html'))
+//})
+//
+
+app.use(express.static(__dirname))
+
+app.listen(8081)
